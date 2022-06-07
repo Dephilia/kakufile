6c007df7fbf5bea6e4e780c43a06b759bb19018f
Dephilia <me@dephilia.moe>
2022-06-08 00:15:28 +0800

    feat: Add keymap for lsp
    For nvim

--- nvim/lua/lsp/setup.lua
+++ nvim/lua/lsp/setup.lua
@@ -6,17 +6,18 @@
 -- Distributed under terms of the MIT license.
 --
 
+local servers = {
+  'pyright',
+  'clangd',
+  'sumneko_lua',
+  'rust_analyzer',
+  'bashls',
+  'html',
+  'tsserver',
+}
 
 require("nvim-lsp-installer").setup({
-    ensure_installed = {
-        'pyright',
-        'clangd',
-        'sumneko_lua',
-        'rust_analyzer',
-        'bashls',
-        'html',
-        'tsserver',
-    },
+    ensure_installed = servers,
     automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
     ui = {
         icons = {
@@ -29,6 +30,47 @@ require("nvim-lsp-installer").setup({
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+-- Loop config
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
 
 require'lspconfig'.sumneko_lua.setup {
   settings = {
@@ -51,24 +93,5 @@ require'lspconfig'.sumneko_lua.setup {
       },
     },
   },
-  capabilities = capabilities
 }
 
-require'lspconfig'.clangd.setup{
-  capabilities = capabilities
-}
-require'lspconfig'.rust_analyzer.setup{
-  capabilities = capabilities
-}
-require'lspconfig'.pyright.setup{
-  capabilities = capabilities
-}
-require'lspconfig'.bashls.setup{
-  capabilities = capabilities
-}
-require'lspconfig'.html.setup{
-  capabilities = capabilities
-}
-require'lspconfig'.tsserver.setup{
-  capabilities = capabilities
-}
